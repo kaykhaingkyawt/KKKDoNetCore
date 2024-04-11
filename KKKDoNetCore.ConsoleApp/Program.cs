2d4916cc77bc90480054d982d3a9ace3b2e2975a
Kay Khaing Kyawt <kaykhaingkyawt214@gmail.com>
2024-04-11 00:39:20 -0700

    [Completed] Read AdoDoNet

--- KKKDoNetCore.ConsoleApp/Program.cs
+++ KKKDoNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+
+//Connect Database
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "."; //Server name
+stringBuilder.InitialCatalog = "KKKDoNetCore"; //database name
+stringBuilder.UserID = "sa"; // Server Id
+stringBuilder.Password = "sa@123";// Server password
+
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+connection.Open();
+Console.WriteLine("Connection Open.");
+
+string query = "select * from tbl_blog";
+SqlCommand cmd = new SqlCommand(query, connection); // select * from command call tar
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd); // new qurery call p command write lo ya aung load tar
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
+
+connection.Close(); 
+Console.WriteLine("Connection Close.");
+
+foreach(DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id => " + dr["BlogId"]);
+    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+    Console.WriteLine("--------------------------------------------");
+}
+
+Console.ReadKey();
